0e64edf3bcdbf1c1d69ea01bc1e0d524d0cfce1e
akirago <akirasyoku1234@gmail.com>
2019-12-14 15:57:10 +0900

    change style

--- app/src/main/java/gotitapi/haro_scoutar/MainActivity.kt
+++ app/src/main/java/gotitapi/haro_scoutar/MainActivity.kt
@@ -1,12 +1,24 @@
 package gotitapi.haro_scoutar
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import gotitapi.haro_scoutar.ar.ArActivity
+import gotitapi.haro_scoutar.register.RegisterActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        move_haro_button.setOnClickListener {
+            startActivity(Intent(this, ArActivity::class.java))
+        }
+
+        move_register_button.setOnClickListener {
+            startActivity(Intent(this, RegisterActivity::class.java))
+        }
     }
 }

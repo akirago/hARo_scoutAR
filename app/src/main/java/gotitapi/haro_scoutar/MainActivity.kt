197bef193fdda6290d9d8daa751f1ecd51a91f03
Akira-Hashimoto <akirasyoku1234@gmail.com>
2019-12-14 11:53:10 +0900

    first commit

---
+++ app/src/main/java/gotitapi/haro_scoutar/MainActivity.kt
@@ -0,0 +1,12 @@
+package gotitapi.haro_scoutar
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}

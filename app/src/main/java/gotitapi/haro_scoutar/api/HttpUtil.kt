112ceb645f99d977e1efd89de82f831a99d68da0
akirago <akirasyoku1234@gmail.com>
2019-12-14 19:05:00 +0900

    create api function and data class

--- app/src/main/java/gotitapi/haro_scoutar/api/HttpUtil.kt
+++ app/src/main/java/gotitapi/haro_scoutar/api/HttpUtil.kt
@@ -1,41 +1,36 @@
 package gotitapi.haro_scoutar.api
 
 import android.graphics.Bitmap
-import android.graphics.BitmapFactory
-import android.util.Base64
 import android.util.Log
-import okhttp3.RequestBody.Companion.toRequestBody
+import gotitapi.haro_scoutar.data.RequestData
 import okhttp3.MediaType.Companion.toMediaTypeOrNull
 import okhttp3.OkHttpClient
 import okhttp3.Request
+import okhttp3.RequestBody.Companion.toRequestBody
 import okhttp3.Response
-import java.io.ByteArrayOutputStream
 import java.io.IOException
 
 object HttpUtil {
     private val client = OkHttpClient()
 
-    fun httpGet(url: String): String? {
-        val request = Request.Builder()
-            .url(url)
-            .build()
+//    fun httpGet(url: String): String? {
+//        val request = Request.Builder()
+//            .url(url)
+//            .build()
+//
+//        val response: Response
+//        try {
+//            response = client.newCall(request).execute()
+//        } catch (e: Exception) {
+//            throw IOException()
+//        }
+//        return response.body?.string()
+//    }
 
-        val response: Response
-        try {
-            response = client.newCall(request).execute()
-        } catch (e: Exception) {
-            throw IOException()
-        }
-        return response.body?.string()
-    }
-
-    fun getTapiocaImage(url: String, bitmap: Bitmap): Bitmap {
-        val stream = ByteArrayOutputStream()
-        bitmap.compress(Bitmap.CompressFormat.PNG, 0, stream)
-        val byteArray = stream.toByteArray()
-        val encodedImage = Base64.encodeToString(byteArray, Base64.DEFAULT)
-
-        val requestBody = encodedImage.toRequestBody("plain/text".toMediaTypeOrNull())
+    fun registerProfile(requestData: RequestData): String {
+        val url = ""
+        val requestBody =
+            requestData.jsonObject.toString().toRequestBody("application/json".toMediaTypeOrNull())
 
         val request = Request
             .Builder()
@@ -49,15 +44,37 @@ object HttpUtil {
             response = client.newCall(request).execute()
             Log.d("httpconnection success", response.body?.string())
         } catch (e: Exception) {
-            Log.e("httpconnection error",e.message + e.cause)
+            Log.e("httpconnection error", e.message + e.cause)
             throw IOException()
         }
-
-        val jpgarr = Base64.decode(encodedImage, Base64.DEFAULT)
-        val bitmap = BitmapFactory.decodeByteArray(jpgarr, 0, jpgarr.size)
-        return bitmap
+        return response.body!!.string()
     }
 
 
+    fun getProfile(bitmap: Bitmap): Bitmap {
+        val url = ""
+
+        val encodedImage = bitmap.toBase64()
+        val requestBody = encodedImage.toRequestBody("application/json".toMediaTypeOrNull())
+
+        val request = Request
+            .Builder()
+            .url(url)
+            .post(requestBody)
+            .build()
+
+
+        val response: Response
+        try {
+            response = client.newCall(request).execute()
+            Log.d("httpconnection success", response.body?.string())
+        } catch (e: Exception) {
+            Log.e("httpconnection error", e.message + e.cause)
+            throw IOException()
+        }
+
+
+        return response.body!!.string().fromBase64toBitmap()
+    }
 
 }
--- app/src/main/java/gotitapi/haro_scoutar/register/RegisterActivity.kt
+++ app/src/main/java/gotitapi/haro_scoutar/register/RegisterActivity.kt
@@ -1,10 +1,16 @@
 package gotitapi.haro_scoutar.register
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.graphics.drawable.toBitmap
 import gotitapi.haro_scoutar.R
+import gotitapi.haro_scoutar.api.HttpUtil
+import gotitapi.haro_scoutar.data.RequestData
+import kotlinx.android.synthetic.main.register_view.*
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.MainScope
+import kotlinx.coroutines.launch
 
 class RegisterActivity : AppCompatActivity(), CoroutineScope by MainScope() {
 
@@ -12,5 +18,27 @@ class RegisterActivity : AppCompatActivity(), CoroutineScope by MainScope() {
         super.onCreate(savedInstanceState)
 
         setContentView(R.layout.register_view)
+
+        send_button.setOnClickListener {
+            val data = RequestData(
+                name_edit_text.text.toString(),
+                photo_image_view.drawable.toBitmap(),
+                twitter_edit_text.text.toString(),
+                github_edit_text.text.toString()
+            )
+            launch {
+                runCatching {
+                    HttpUtil.registerProfile(data)
+                }.onSuccess {
+                    if (it.isBlank()) {
+                        finish()
+                    } else {
+                        Toast.makeText(this@RegisterActivity, it, Toast.LENGTH_LONG).show()
+                    }
+                }.onFailure {
+
+                }
+            }
+        }
     }
 }
